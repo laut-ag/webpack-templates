b02d2a60d8bfb589e1e9efbcac4b57a6ded822e7
Vern Hockney <vern@laut.fm>
2019-02-05 13:35:37 +0100

    fix script error bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,5 +6,5 @@ if [[ -f "$WD/webpack.config.js" ]]
 then
     echo -e "$WD/webpack.config.js already exists. Not copying." >&2
 else
-    cp ./webpack.config.js $WD
+    cp ./index.js $WD/webpack.config.js
 fi
